0f203cb588d5c22eea294d11b5227f8cf9101e12
hexa <dianatikian@gmail.com>
2024-05-13 12:20:08 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -21,8 +21,8 @@ class Polynomial {
 
         double[] coeff = new double[max];
 
-        for (int i = 0; i < min; i++) {
-            if (this.coeff.length <= i) coeff[i] = that.coeff[i];
+        for (int i = 0; i < max; i++) {
+            if      (this.coeff.length <= i) coeff[i] = that.coeff[i];
             else if (that.coeff.length <= i) coeff[i] = this.coeff[i];
             else coeff[i] = this.coeff[i] + that.coeff[i];
         }
